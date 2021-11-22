a874ecef4173a6f599e9dc5d0ffc752b4f733d04
orlandod1 <orlandoldiaz@uniguajira.edu.co>
2021-11-22 17:24:05 -0500

    primer commit

---
+++ appcons/apps/inventario/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models.fields import IntegerField
+
+# Create your models here.
+
+class inventario(models.Model):
+
+    in_nombre = models.CharField(max_length=70,verbose_name ='Nombre del material')
+    in_costo = models.IntegerField(max_length=13,verbose_name='Costo total ')
+    
