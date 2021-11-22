ac04d236ccd032e6c3490abd09d82a74395835f2
orlandod1 <orlandoldiaz@uniguajira.edu.co>
2021-11-22 17:33:55 -0500

    creración del modelo compra

--- appcons/apps/compra/models.py
+++ appcons/apps/compra/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Compra (models.Model):
+
+    co_cantidad = models.IntegerField(max_length=11,verbose_name="Cantidad")
+
+
+    
